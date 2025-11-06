1ceda2d21e4b79d045c8fdc9347a34f8afbe8be9
martimcaridade <martimcaridade@gmail.com>
2025-11-06 15:38:13 +0000

    loop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 print("Hello, Astro Pi!")
 
 #Se conseguirem ver este código, parabéns! Já sabes usar as funções básicas do git!
-nome1=input("Introduza o nome do próximo astronauta?")
-nome2=input("Introduza o nome do próximo astronauta?")
-nome3=input("Introduza o nome do próximo astronauta?")
-nome4=input("Introduza o nome do próximo astronauta?")
-nome5=input("Introduza o nome do próximo astronauta?")
+
+for i in range(nomes):
+    print(i, 'é o mais novo astronauta da Esa')
