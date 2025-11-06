dee8c72e172e77130e2ac8382d411208beb19652
dianamatos111 <diana.i.a.matos.111@gmail.com>
2025-11-06 15:31:49 +0000

    pedido de input

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 print("Hello, Astro Pi!")
 
 #Se conseguirem ver este código, parabéns! Já sabes usar as funções básicas do git!
+nome1=input("Introduza o nome do próximo astronauta?")
+nome2=input("Introduza o nome do próximo astronauta?")
+nome3=input("Introduza o nome do próximo astronauta?")
+nome4=input("Introduza o nome do próximo astronauta?")
+nome5=input("Introduza o nome do próximo astronauta?")
