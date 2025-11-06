5c7f63d33d392e389db7ba349327aaeb422b0a0f
Ordep-tech <you@example.com>
2025-11-06 15:40:17 +0000

    trying to fix git issue

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 print("Hello, Astro Pi!")
 
 #Se conseguirem ver este código, parabéns! Já sabes usar as funções básicas do git!
+<<<<<<< HEAD
 
 nomes = [nome1 , nome2 , nome3 , nome4 , nome5 ]
+=======
+nome1=input("Introduza o nome do próximo astronauta?")
+nome2=input("Introduza o nome do próximo astronauta?")
+nome3=input("Introduza o nome do próximo astronauta?")
+nome4=input("Introduza o nome do próximo astronauta?")
+nome5=input("Introduza o nome do próximo astronauta?")
+>>>>>>> dee8c72e172e77130e2ac8382d411208beb19652
