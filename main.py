c1ca3fdf11a74e75d11e32f8496e35b08d3b86ab
José Ruy Sá Ribeiro <joseruysaribeiro@icloud.com>
2025-11-06 15:41:47 +0000

    new file

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ print("Hello, Astro Pi!")
 #Se conseguirem ver este código, parabéns! Já sabes usar as funções básicas do git!
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 nomes = [nome1 , nome2 , nome3 , nome4 , nome5 ]
 =======
 for i in range(nomes):
     print(i, 'é o mais novo astronauta da Esa')
 >>>>>>> 1ceda2d21e4b79d045c8fdc9347a34f8afbe8be9
+=======
+>>>>>>> 2847ef6 (new file)
