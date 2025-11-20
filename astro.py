d3aab7a62f7899465accd1862784328110161152
José Ruy Sá Ribeiro <joseruysaribeiro@icloud.com>
2025-11-20 14:40:50 +0000

    some things

--- astro.py
+++ astro.py
@@ -1,8 +1,22 @@
 import sys
 from astro_pi_orbit import ISS
+import time
 
 iss = ISS()
 
-print(iss.coordinates())
-print(iss.coordinates().latitude.degrees)
+coord1 = {
+    "coord": iss.coordinates(),
+    "time": time.localtime()
+}
 
+time.sleep(2)
+coord2 = {
+    "coord": iss.coordinates(),
+    "time": time.localtime()
+}
+
+print(f"Coordinate 1: {coord1['coord']} at time {time.strftime('%Y-%m-%d %H:%M:%S', coord1['time'])}")
+print(f"Coordinate 2: {coord2['coord']} at time {time.strftime('%Y-%m-%d %H:%M:%S', coord2['time'])}")
+
+
+print(coord1)
